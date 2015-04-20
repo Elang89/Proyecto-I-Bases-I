7bd0dd1ea5c22b8092727f83c7a631376b9aef88
Elang89 <elang8899@gmail.com>
2015-04-20 11:39:41 -0600

    arreglo de interfaz

--- Project/edit-user.php
+++ Project/edit-user.php
@@ -1,10 +1,9 @@
 <?php include'header.php';?>
 <!-- banner -->
-<script src="javascript/findInputs.js"></script>
 <div class="inside-banner">
   <div class="container">
-    <span class="pull-right"><a href="#">Home</a> / Edit User</span>
-    <h2>Edit User</h2>
+    <span class="pull-right"><a href="index.php">Home</a> / User Profile</span>
+    <h2>User Profile</h2>
   </div>
 </div>
 <!-- banner -->
@@ -73,44 +72,28 @@
 ?>
 <div class="container">
   <div class="spacer">
-    <div class="row register">
-      <div class="col-lg-6">
-        <legend>Photo</legend>
+    <div class="row contact">
+      <div class="col-lg-6 col-sm-6">
+        <legend>Profile Picture</legend>
         <img src="images/Users/user_without_photo.png" class="img-responsive img-circle" alt="properties"/>
         <input type="file" name="photo" title="Photo"/>
         <input id="user_username" type="text" class="form-control" placeholder="Username" name="form_name" maxlength="12" value="<?php echo $_SESSION['name']?>" readonly>
-        <h4>Change password</h4>
-        <div class="row">
-          <div class="col-lg-6">
-            <input id="user_old_password" type="password" class="form-control" placeholder="Old Password" name="form_phone" maxlength="16" value="">
-          </div>
+		<div class="col-lg-6">
+			<button id="upload_image" type="button" class="btn btn-success">Upload</button>
+	    </div>
         </div>
-        <div class="row">
-          <div class="col-lg-6">
-            <input id="user_password" type="password" class="form-control" placeholder="New Password" name="form_phone" maxlength="16" value="">
-          </div>
-          <div class="col-lg-6">
-            <input id="user_re_password" type="password" class="form-control" placeholder="Confirm Password" name="form_phone" maxlength="16" value="">
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-6">
+      <div class="col-lg-6 col-sm-6">
         <input id="user_first_name" type="text" class="form-control" name="form_name" maxlength="20" readonly value="<?php echo $firstName?>">
         <input id="user_first_lastname" type="text" class="form-control" name="form_name" maxlength="16" readonly value="<?php echo $lastName?>">
         <input id="user_second_lastname" type="text" class="form-control" name="form_name" maxlength="16" readonly value="<?php echo $secondLastName?>">
         <input id="user_email" type="text" class="form-control" name="form_email" maxlength="30" readonly value="<?php echo $primaryEmail?>">
         <input id="user_phone" type="text" class="form-control" name="form_email" maxlength="8" readonly value="<?php echo $primaryPhone?>">
-        <legend>Which type of user would you want to be?</legend>
-        <input type="checkbox" name="animal" value="" style="height: 20px" checked>
-        <label>Adopter</label> <br />
-        <input type="checkbox" name="animal" value="" style="height: 20px">
-        <label>Rescuer</label> <br />
         <div class="row">
           <div class="col-lg-6">
-            <button id="user_phones_butt" type="button" class="btn btn-success" data-toggle="modal" data-target="#user_phones">Edit Phones</button>
+            <button id="user_phones_butt" type="button" class="btn btn-success" data-toggle="modal" data-target="#user_phones">View Phones</button>
           </div>
           <div class="col-lg-6">
-            <button id="user_email_butt" type="button" class="btn btn-success" data-toggle="modal" data-target="#user_Email">Edit Email</button>
+            <button id="user_email_butt" type="button" class="btn btn-success" data-toggle="modal" data-target="#user_Email">View Email</button>
           </div>
         </div>
 		<br>
@@ -127,14 +110,7 @@
         </div>
       </div>
     </div>
-	<br>
-    <div class="row register">
-      <div class="pull-right">
-        <button id="Submit_User" type="button" class="btn btn-success" name="Submit" >Save Changes</button>
-      </div>
-    </div>
   </div>
-</div>
 <!-- Modal numbers-->
 <div id="user_phones" class="modal fade">
   <div class="modal-dialog">
@@ -142,18 +118,14 @@
       <div class="row">
         <div class="col-sm-6">
           <h4>Phone numbers of <?php echo $_SESSION['name']?></h4>
-          <input id="user_phone" type="text" class="form-control" placeholder="Phone 1" name="form_name" maxlength="20" value="">
-          <input id="user_phone" type="text" class="form-control" placeholder="Phone 2" name="form_name" maxlength="20" value="">
-          <input id="user_phone" type="text" class="form-control" placeholder="Phone 3" name="form_name" maxlength="20" value="">
+          <input id="user_phone" type="text" class="form-control" placeholder="Phone 1" name="form_name" maxlength="20" value="" readonly>
+          <input id="user_phone" type="text" class="form-control" placeholder="Phone 2" name="form_name" maxlength="20" value="" readonly>
+          <input id="user_phone" type="text" class="form-control" placeholder="Phone 3" name="form_name" maxlength="20" value="" readonly>
         </div>
       </div>
       <div class="row">
-        <div class="pull-right">
           <div class="col-lg-6">
-            <button id="user_phones_cancel" type="button" class="btn btn-success">Cancel</button>
-          </div>
-          <div class="col-lg-6">
-            <button id="user_phones_save" type="button" class="btn btn-success">Save</button>
+            <button id="user_phones_close" type="button" class="btn btn-success">Close</button>
           </div>
         </div>
       </div>
@@ -169,18 +141,14 @@
       <div class="row">
         <div class="col-sm-6">
           <h4>Emails of <?php echo $_SESSION['name']?></h4>
-          <input id="user_email" type="text" class="form-control" placeholder="Email 1" name="form_name" maxlength="20" value="">
-          <input id="user_email" type="text" class="form-control" placeholder="Email 2" name="form_name" maxlength="20" value="">
-          <input id="user_email" type="text" class="form-control" placeholder="Email 3" name="form_name" maxlength="20" value="">
+          <input id="user_email" type="text" class="form-control" placeholder="Email 1" name="form_name" maxlength="20" value="" readonly>
+          <input id="user_email" type="text" class="form-control" placeholder="Email 2" name="form_name" maxlength="20" value="" readonly>
+          <input id="user_email" type="text" class="form-control" placeholder="Email 3" name="form_name" maxlength="20" value="" readonly>
         </div>
       </div>
       <div class="row">
-        <div class="pull-right">
           <div class="col-lg-6">
-            <button id="user_Email_Cancel" type="button" class="btn btn-success">Cancel</button>
-          </div>
-          <div class="col-lg-6">
-            <button id="user_Email_Save" type="button" class="btn btn-success">Save</button>
+            <button id="user_email_close" type="button" class="btn btn-success">Close</button>
           </div>
         </div>
       </div>

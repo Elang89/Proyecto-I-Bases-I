21caa1f041cbb1729b15f25472b2fea935a0cc72
miumiu997 <miuyinyong@yahoo.com>
2015-04-24 18:14:54 -0600

    agregar interfaz myPets

---
+++ Project/myPets.php
@@ -0,0 +1,50 @@
+<?php include'header.php';?>
+<!-- banner -->
+<div class="inside-banner">
+  <div class="container">
+    <span class="pull-right"><a href="index.php">Home</a> / User Results</span>
+    <h2>My Pets</h2> 
+  </div>
+</div>
+<!-- banner -->
+
+<div class="container">
+  <div class="properties-listing spacer">
+    <div class="row">
+      <div class="col-lg-3 col-sm-4 ">
+        <div class="hot-properties hidden-xs">
+          <div class="row">
+            <div class="col-lg-4 col-sm-5"><img src="images/properties/9.jpg" class="img-responsive img-circle" alt="properties"></div>
+			<div class="col-lg-8 col-sm-7">
+				<h5>Golden Retriever</h5>
+			</div>
+          </div>
+		   <div class="row">
+            <div class="col-lg-4 col-sm-5"><img src="images/properties/4.jpg" class="img-responsive img-circle" alt="properties"></div>
+			<div class="col-lg-8 col-sm-7">
+				<h5>Siberian Husky</h5>
+			</div>
+          </div>
+		  <div class="row">
+            <div class="col-lg-4 col-sm-5"><img src="images/properties/Weimeraner.jpg" class="img-responsive img-circle" alt="properties"></div>
+			<div class="col-lg-8 col-sm-7">
+				<h5>Weimeraner</h5>
+			</div>
+          </div>
+		  <div class="row">
+            <div class="col-lg-4 col-sm-5"><img src="images/properties/Ridgeback.jpg" class="img-responsive img-circle" alt="properties"></div>
+			<div class="col-lg-8 col-sm-7">
+				<h5>Ridgeback</h5>
+			</div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-9 col-sm-8">
+        <div class="row">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include'footer.php';?>

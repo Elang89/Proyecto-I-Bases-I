d5471ee22033fd026276f16cefb61de9598545df
Elang89 <elang8899@gmail.com>
2015-04-28 18:36:37 -0600

    Revert "pet search"
    This reverts commit 7f5a11092aafe156b1a693aa935cd2f0e1fc602f.

--- SQL scripts/40.pet_search-package.sql
+++ SQL scripts/40.pet_search-package.sql
@@ -141,33 +141,34 @@ CREATE OR REPLACE PACKAGE BODY pet_search_package AS
        from dbadmin.petSpace SP
        where SP.pet_space = SPACE; 
 
-      
-      FUNCTION find_myAdoptedPets(user_id person.person_id%type)
-       RETURN SYS_REFCURSOR
-       IS
-          my_pets SYS_REFCURSOR;
-       BEGIN
-        OPEN my_pets FOR select pet.pet_code, pet_name, pet_type_name, pet_race_name, pet_cond_name, pet_energy_level, pet_learn_skill, vet_name, person_name, petlocation, petnotes, petabandondescription, pet_space, pet_treatment, pet_color, pet_sickness_name, pet_med_name, image
-        from pet, pettype, petrace, petCondition, petSize,petEnergy, petlearningskill, veterinary, person, petSpace, pettreatments, petcolor, petsickness, petmedicine, pet_photo_storage
-        where adoptant_id = user_id
-        AND pettype.pet_type_code = pet.pet_type_code
-        AND petrace.pet_race_code = pet.pet_race_code
-        AND pet.pet_cond_code = petcondition.pet_cond_code
-        AND pet.pet_size_code = petsize.pet_size_code
-        AND pet.pet_energy_code = petEnergy.Pet_Energy_Code
-        AND pet.pet_learn_code = petlearningskill.pet_learn_code
-        AND pet.vet_code = veterinary.vet_code
-        AND pet.owner_id = person.person_id
-        AND pet.petspace_id = petSpace.Pet_Space_Code
-        AND pet.pettreatments_id = pettreatments.pet_treatment_code
-        AND pet.pet_color_code = petcolor.pet_color_code
-        AND pet.pet_sickness_code = petsickness.pet_sickness_code
-        AND pet.pet_medicine_code = petmedicine.pet_med_code
-        AND pet.pet_code = pet_photo_storage.pet_code;
-        RETURN my_pets;
-	   EXCEPTION
-		WHEN NO_DATA_FOUND THEN 
-			RETURN NULL;
-       END;
+    OPEN pet_search_result FOR select pet.pet_code,pet_name, pet_type_name, pet_race_name, pet_cond_name, pet_energy_level, pet_learn_skill, vet_name, person_name, petlocation, petnotes, petabandondescription, pet_space, pet_treatment, pet_color, pet_sickness_name, pet_med_name, image
+    from pet, pettype, petrace, petCondition, petSize,petEnergy, petlearningskill, veterinary, person, petSpace, pettreatments, petcolor, petsickness, petmedicine, pet_photo_storage
+    WHERE pettype.pet_type_code = type_id
+    AND petrace.pet_race_code = breed_id  
+    AND pet.pet_color_code = color_id
+    AND pet.pet_size_code = size_id  
+    AND pet.pet_learn_code = TS_id 
+    AND pet.pet_energy_code = energy_id 
+    AND pet.petspace_id = space_id 
+    AND pettype.pet_type_code = pet.pet_type_code 
+    AND petrace.pet_race_code = pet.pet_race_code 
+    AND pet.pet_cond_code = petcondition.pet_cond_code 
+    AND pet.pet_size_code = petsize.pet_size_code 
+    AND pet.pet_energy_code = petEnergy.Pet_Energy_Code 
+    AND pet.pet_learn_code = petlearningskill.pet_learn_code 
+    AND pet.vet_code = veterinary.vet_code 
+    AND pet.owner_id = person.person_id 
+    AND pet.petspace_id = petSpace.Pet_Space_Code 
+    AND pet.pettreatments_id = pettreatments.pet_treatment_code 
+    AND pet.pet_color_code = petcolor.pet_color_code 
+    AND pet.pet_sickness_code = petsickness.pet_sickness_code 
+    AND pet.pet_medicine_code = petmedicine.pet_med_code
+    AND pet.pet_code = pet_photo_storage.pet_code;
+        RETURN pet_search_result; 
+
+      EXCEPTION 
+        WHEN NO_DATA_FOUND THEN 
+          RETURN null;
+      END; 
 
 END pet_search_package;
